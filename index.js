f7fa397f0def41f75113a6cb9031350e7ad73ec0
keshri95 <keshri.ashish1@gmail.com>
2022-05-08 18:11:31 +0530

    responsive all the width

---
+++ index.js
@@ -0,0 +1,13 @@
+// form validaton
+
+// const getId = document.getElementById();
+
+// let userName, userEmail, subject, userFeefback;
+
+let userName = document.getElementById("name");
+let userEmail = document.getElementById("email");
+let userSub = document.getElementById("subject");
+let userFeed = document.getElementById("feedback");
+
+
+

d6fe8f2b773bed8b5cd6208f695305b581f23957
keshri95 <keshri.ashish1@gmail.com>
2022-05-14 15:39:29 +0530

    form-validation

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 // form validaton
 
-// const getId = document.getElementById();
+let getId = (id) => document.getElementById(id);
+// let getClasses = (classes) => document.getElementsByClassName(classes);
 
+// let inputFeilds = getId(id,classes,message);
 // let userName, userEmail, subject, userFeefback;
 
-let userName = document.getElementById("name");
-let userEmail = document.getElementById("email");
-let userSub = document.getElementById("subject");
-let userFeed = document.getElementById("feedback");
+let userName = getId("name"),
+  userEmail = getId("email"),
+  userSub = getId("subject"),
+  userFeed = getId("feedback"),
+  form = getId("formValidation");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
+  ValidaeFieleds(userName, 0, "Name can not empty");
+  ValidaeFieleds(userEmail, 1, "Email can bot empty");
+  ValidaeFieleds(userSub, 2, "subject cannot be empty");
+  ValidaeFieleds(userFeed, 3, "feedback can not be empty");
+});
 
+let ValidaeFieleds = (id) => {
+  if (id.value.trim() === "") {
+    console.log("error");
+  } else {
+    console.log("no error");
+  }
+};
